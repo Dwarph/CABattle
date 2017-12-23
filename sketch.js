4fdf87c42f55065f6bec0bbdb9e28fb2333a7a70
Dwarph <pipturner@outlook.com>
2017-12-23 02:10:48 +0000

    Changed Scale & colour of initial CA. Sleep time!

--- sketch.js
+++ sketch.js
@@ -5,9 +5,9 @@ var automataManager;
 function setup() {
   // put setup code here
   createCanvas(windowWidth, windowHeight);
-  automataList[0] = new Automata(1, 1, 1, '#fcfc00');
-  automataList[1] = new Automata(100, 100, 100, '#7acc57');
-  gridBuilder = new GridBuilder(10, 10, 50);
+  automataList[0] = new Automata(1, 1, 1, '#8eb9ff');
+  automataList[1] = new Automata(100, 100, 100, '#d3e4ff');
+  gridBuilder = new GridBuilder(500, 500, 3);
   gridBuilder.generateGrid();
   automataManager = new AutomataManager(500, gridBuilder, 100);
 
@@ -16,9 +16,12 @@ function setup() {
 function draw() {
   // put drawing code here
   automataManager.battleLoop();
+  if (mouseIsPressed) {
+    gridBuilder.clickCheck();
+  }
 
 }
 
 function mousePressed() {
-  gridBuilder.clickCheck();
+
 }
